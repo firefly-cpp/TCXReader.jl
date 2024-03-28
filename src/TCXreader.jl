02bd99ba4079a1f88164c45e605632674c704737
HlisTilen <tilen.hlis@gmail.com>
2024-03-28 01:00:17 +0100

    fix: Optimize code in TCXreader

--- src/TCXreader.jl
+++ src/TCXreader.jl
@@ -2,58 +2,44 @@ module TCXreader
 
 using EzXML
 using Dates
+
 include("TCXTrackPoint.jl")
 using .TrackPoint: TCXTrackPoint
 
 export loadTCXFile
 
-const GARMIN_XML_SCHEMA = "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"
-const GARMIN_XML_EXTENSIONS = "http://www.garmin.com/xmlschemas/ActivityExtension/v2"
+const NS_MAP = Dict(
+    "g" => "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2",
+    "ns3" => "http://www.garmin.com/xmlschemas/ActivityExtension/v2"
+)
 
-function parseTCXTrackPoint(node::EzXML.Node)
+function parseDateTime(timeStr::String)
+    isempty(timeStr) ? DateTime(1, 1, 1) : DateTime(timeStr, dateformat"yyyy-mm-ddTHH:MM:SS.sssZ")
+end
 
-    # Extract Time
-    timeStr = nodecontent.(findall(".//g:Time", node, ["g" => namespace(node)]))[1]
-    time = timeStr != "" ? DateTime(timeStr, dateformat"yyyy-mm-ddTHH:MM:SS.sssZ") : DateTime(1, 1, 1)
+function parseValue(node::Union{Nothing,EzXML.Node}, parser::Function=identity)
+    node === nothing ? nothing : parser(nodecontent(node))
+end
 
-    # Extract Latitude and Longitude
-    latitudeNode = findfirst(".//g:Position/g:LatitudeDegrees", node, ["g" => namespace(node)])
-    longitudeNode = findfirst(".//g:Position/g:LongitudeDegrees", node, ["g" => namespace(node)])
-    latitude = latitudeNode !== nothing ? parse(Float64, nodecontent(latitudeNode)) : nothing
-    longitude = longitudeNode !== nothing ? parse(Float64, nodecontent(longitudeNode)) : nothing
-
-    # Extract AltitudeMeters
-    altitudeMetersNode = findfirst(".//g:AltitudeMeters", node, ["g" => namespace(node)])
-    altitude_meters = altitudeMetersNode !== nothing ? parse(Float64, nodecontent(altitudeMetersNode)) : nothing
-
-    # Extract DistanceMeters
-    distanceMetersNode = findfirst(".//g:DistanceMeters", node, ["g" => namespace(node)])
-    distance_meters = distanceMetersNode !== nothing ? parse(Float64, nodecontent(distanceMetersNode)) : nothing
-
-    # Extract HeartRateBpm
-    heartRateBpmNode = findfirst(".//g:HeartRateBpm/g:Value", node, ["g" => namespace(node)])
-    heart_rate_bpm = heartRateBpmNode !== nothing ? parse(Int, nodecontent(heartRateBpmNode)) : nothing
-
-    # Extract Speed
-    speedNode = findfirst(".//ns3:TPX/ns3:Speed", node, ["ns3" => "http://www.garmin.com/xmlschemas/ActivityExtension/v2"])  
-    speed = speedNode !== nothing ? parse(Float64, nodecontent(speedNode)) : nothing
+function parseTCXTrackPoint(node::EzXML.Node)    
+    time = parseDateTime(nodecontent(findfirst(".//g:Time", node, NS_MAP)))
+    latitude = parseValue(findfirst(".//g:Position/g:LatitudeDegrees", node, NS_MAP), x -> parse(Float64, x))
+    longitude = parseValue(findfirst(".//g:Position/g:LongitudeDegrees", node, NS_MAP), x -> parse(Float64, x))
+    altitude_meters = parseValue(findfirst(".//g:AltitudeMeters", node, NS_MAP), x -> parse(Float64, x))
+    distance_meters = parseValue(findfirst(".//g:DistanceMeters", node, NS_MAP), x -> parse(Float64, x))
+    heart_rate_bpm = parseValue(findfirst(".//g:HeartRateBpm/g:Value", node, NS_MAP), x -> parse(Int, x))
+    speed = parseValue(findfirst(".//ns3:TPX/ns3:Speed", node, NS_MAP), x -> parse(Float64, x))
 
     return TCXTrackPoint(time, latitude, longitude, altitude_meters, distance_meters, heart_rate_bpm, speed)
-
 end
 
 function loadTCXFile(filepath::String)
     doc = readxml(filepath)
-
-    trackpoints = findall(".//g:Trackpoint", doc.root, ["g" => GARMIN_XML_SCHEMA])
+    trackpoints = findall(".//g:Trackpoint", doc.root, NS_MAP)
 
     println("Found ", length(trackpoints), " trackpoints.")
 
-    parsed_trackpoints = []
-
-    for tp in trackpoints
-        push!(parsed_trackpoints, parseTCXTrackPoint(tp))
-    end
+    parsed_trackpoints = [parseTCXTrackPoint(tp) for tp in trackpoints]
 
     return parsed_trackpoints
 end
